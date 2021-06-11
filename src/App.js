4d4c3e382c750d144edf89a9f8e479a7da7987d8
matheus <matheuscomteaga@hotmail.com>
2021-06-11 13:38:56 -0300

    usando api com axios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import './styles.css';
-
-//https://sujeitoprogramador.com/r-api/?api=filmes/
+import "./styles.css";
+import Routes from "./routes";
 
 export default function App() {
   return (
-    <div className="App">
-     <h1>Filmaria</h1>
+    <div className="app">
+      <Routes />
     </div>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,8 @@
+
+export default function App() {
+  return (
+    <div>
+     <h1>Página Home</h1>
+    </div>
+  );
+}
