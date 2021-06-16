096a664d7b7d8bdb3f54025d5927cd22cfbe2f93
matheus <matheuscomteaga@hotmail.com>
2021-06-16 16:57:57 -0300

    adicionado not found

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Filme from './pages/Filme';
 import Favoritos from './pages/Favoritos';
+import Erro from './pages/Erro';
 
 const Routes = () => {
   return (
@@ -12,6 +13,7 @@ const Routes = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/filme/:id" component={Filme} />
         <Route exact path="/favoritos" component={Favoritos} />
+        <Route path="*" component={Erro} />
       </Switch>
     </BrowserRouter>
   );
